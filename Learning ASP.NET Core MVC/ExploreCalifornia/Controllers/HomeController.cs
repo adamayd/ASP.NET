052957b38dd1897ce07a3d752d30da2548222ee7
Adam Ayd <adam.ayd@gmail.com>
2018-11-18 23:58:50 -0500

    added base controllers with parameters

---
+++ Learning ASP.NET Core MVC/ExploreCalifornia/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.Controllers
+{
+    public class HomeController
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "Hello ASP.NET Core MVC" };
+            //return "Hello ASP.NET Core MVC";
+        }
+    }
+}
--- Learning ASP.NET Core MVC/ExploreCalifornia/Startup.cs
+++ Learning ASP.NET Core MVC/ExploreCalifornia/Startup.cs
@@ -33,6 +33,8 @@ namespace ExploreCalifornia
                 EnableDeveloperExceptions =
                     configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
             });
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +60,11 @@ namespace ExploreCalifornia
                     throw new Exception("ERROR!");
 
                 await next();
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
             });
 
             app.UseFileServer();
